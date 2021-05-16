ec5b810392daf52fc60e42a7bad9538c5cab6430
baonguyenyam <baonguyenyam@gmail.com>
2021-05-16 10:48:18 -0500

    ok

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -6,45 +6,44 @@
         <ul class="nav flex-column">
             <li class="nav-item">
                 <a class="nav-link <?= $active === 'home' ? 'active' : '' ?>" aria-current="page" href="/">
-                    <i class="fa fa-fw fa-home"></i>
-                    Dashboard
+                    <i class="fa fa-fw fa-tachometer-alt me-1"></i> Dashboard
                 </a>
             </li>
-
-        </ul>
-
-        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
-            <span>MORE TOOLS</span>
-        </h6>
-        <ul class="nav flex-column mb-2">
             <li class="nav-item">
                 <a class="nav-link <?= $active === 'keyword' ? 'active' : '' ?>" href="keywork_generator.php">
-                    Keywords generator
+                <i class="fa fa-fw fa-key me-1"></i> Keywords generator
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link <?= $active === 'post' ? 'active' : '' ?>" href="post_generator.php">
-                    Wordpress posts generator
+                <i class="fab fa-fw fa-wordpress-simple me-1"></i> Wordpress posts generator
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link <?= $active === 'html' ? 'active' : '' ?>" href="html_validator.php">
-                    HTML Validator
+                <i class="fas fa-fw fa-code me-1"></i> HTML Validator
                 </a>
             </li>
+        </ul>
+
+        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+            <span>MORE...</span>
+        </h6>
+        <ul class="nav flex-column mb-2">
+
             <li class="nav-item">
                 <a class="nav-link <?= $active === 'chrome' ? 'active' : '' ?>" href="chrome_extensions.php">
-                    Chrome extensions
+                <i class="fab fa-fw fa-chrome me-1"></i> Chrome extensions
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link <?= $active === 'changelog' ? 'active' : '' ?>" href="change_logs.php">
-                    Changelog
+                <i class="fa fa-fw fa-calendar-alt me-1"></i> Changelog
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link <?= $active === 'roadmap' ? 'active' : '' ?>" href="road_map.php">
-                    Roadmap
+                <i class="fa fa-fw fa-sort-amount-up-alt me-1"></i> Roadmap
                 </a>
             </li>
         </ul>
