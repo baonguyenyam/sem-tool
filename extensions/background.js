827b5b3346fc9e7ede1985d95a1ec4095ee55bb8
baonguyenyam <baonguyenyam@gmail.com>
2021-05-16 20:05:53 -0500

    checkv3

--- extensions/background.js
+++ extensions/background.js
@@ -1,28 +1,30 @@
-// Check whether new version is installed
-chrome.runtime.onInstalled.addListener(function(details){
-    if(details.reason == "install"){
-        //alert("This is a first install!");
-        chrome.tabs.create({'url': chrome.extension.getURL('app/welcome.html')}, function(tab) {
-  			// Tab opened.
-		});
-    }else if(details.reason == "update"){
-        //var thisVersion = chrome.runtime.getManifest().version;
-        //alert("Updated from " + details.previousVersion + " to " + thisVersion + "!");
+importScripts('global.js')
 
-        chrome.tabs.create({'url': chrome.extension.getURL('app/welcome.html')}, function(tab) {
-  			// Tab opened.
-		});
-    }
-});
-chrome.browserAction.onClicked.addListener(function (request) {
-})
-chrome.extension.onConnect.addListener(function (request) {
-});
-chrome.runtime.onMessage.addListener(function (request) {
-    if (request.msg == "__build_Grid") {
-        LIFT_APP.grid()
-    }
-    if (request.msg == "__build_Ruler") {
-        LIFT_APP.ruler()
-    }
-});
+// // Check whether new version is installed
+// chrome.runtime.onInstalled.addListener(function(details){
+//     if(details.reason == "install"){
+//         //alert("This is a first install!");
+//         chrome.tabs.create({'url': chrome.extension.getURL('app/welcome.html')}, function(tab) {
+//   			// Tab opened.
+// 		});
+//     }else if(details.reason == "update"){
+//         //var thisVersion = chrome.runtime.getManifest().version;
+//         //alert("Updated from " + details.previousVersion + " to " + thisVersion + "!");
+
+//         chrome.tabs.create({'url': chrome.extension.getURL('app/welcome.html')}, function(tab) {
+//   			// Tab opened.
+// 		});
+//     }
+// });
+// chrome.browserAction.onClicked.addListener(function (request) {
+// })
+// chrome.extension.onConnect.addListener(function (request) {
+// });
+// chrome.runtime.onMessage.addListener(function (request) {
+//     if (request.msg == "__build_Grid") {
+//         LIFT_APP.grid()
+//     }
+//     if (request.msg == "__build_Ruler") {
+//         LIFT_APP.ruler()
+//     }
+// });
