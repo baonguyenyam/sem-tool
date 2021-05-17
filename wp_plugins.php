87512f42cf7e81f307e9862d59311b8204607969
Nguyen Pham <baonguyenyam@gmail.com>
2021-05-17 12:09:01 -0500

    files

--- wp_plugins.php
+++ wp_plugins.php
@@ -47,9 +47,25 @@
                             <div class="d-flex w-100 justify-content-between align-items-start">
                                 <div>
                                     <strong class="d-block text-gray-dark">LIFT Blocks</strong>
-                                    <p class="mb-0">This plugins add new Blocks feature into WWordpress site.</p>
+                                    <p class="mb-0">This plugins add new Blocks feature into Wordpress site.</p>
                                 </div>
                                 <a href="/files/wp-lift-blocks.zip" class="btn btn-sm btn-success">Download</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="d-flex text-muted pt-3">
+                        <div class="bd-placeholder-img flex-shrink-0 me-2 rounded">
+                            <i class="fas fa-archive fa-3x"></i>
+                        </div>
+
+                        <div class="flex-fill pb-3 mb-0 small lh-sm border-bottom">
+                            <div class="d-flex w-100 justify-content-between align-items-start">
+                                <div>
+                                    <strong class="d-block text-gray-dark">LIFT Main</strong>
+                                    <p class="mb-0">This plugins add new LIFT's menu into Wordpress site.</p>
+                                </div>
+                                <a href="/files/wp-lift.zip" class="btn btn-sm btn-success">Download</a>
                             </div>
                         </div>
                     </div>
