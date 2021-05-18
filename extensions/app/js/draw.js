ccd44d9e88a5669861d4ea95faae653a84e3356a
Nguyen Pham <baonguyenyam@gmail.com>
2021-05-18 10:30:47 -0500

    fixed

--- extensions/app/js/draw.js
+++ extensions/app/js/draw.js
@@ -4,6 +4,8 @@ function initDraw(canvas) {
         if (ev.pageX) { //Moz
             mouse.x = ev.pageX + window.pageXOffset;
             mouse.y = ev.pageY + window.pageYOffset;
+            // mouse.x = ev.pageX + window.scrollX;
+            // mouse.y = ev.pageY + window.scrollY;
         } else if (ev.clientX) { //IE
             mouse.x = ev.clientX + document.body.scrollLeft;
             mouse.y = ev.clientY + document.body.scrollTop;
@@ -24,9 +26,9 @@ function initDraw(canvas) {
         setMousePosition(e);
         if (element !== null) {
             element.style.width = Math.abs(mouse.x - mouse.startX) + 'px';
-            element.style.height = Math.abs(mouse.y - mouse.startY) + 'px';
+            element.style.height = Math.abs(mouse.y - mouse.startY  - window.scrollY) + 'px';
             element.style.left = (mouse.x - mouse.startX < 0) ? mouse.x + 'px' : mouse.startX + 'px';
-            element.style.top = (mouse.y - mouse.startY < 0) ? mouse.y + 'px' : mouse.startY + 'px';
+            element.style.top = (mouse.y - mouse.startY < 0) ? mouse.y  - window.scrollY + 'px' : mouse.startY + 'px';
             element.innerHTML = '<span>' + Math.abs(mouse.x - mouse.startX) +' x ' + Math.abs(mouse.y - mouse.startY) + '<span>';
         }
     }
@@ -37,11 +39,11 @@ function initDraw(canvas) {
             canvas.style.cursor = "default";
         } else {
             mouse.startX = mouse.x;
-            mouse.startY = mouse.y;
+            mouse.startY = mouse.y - window.scrollY;
             element = document.createElement('div');
             element.className = 'lift_rectangle'
             element.style.left = mouse.x + 'px';
-            element.style.top = mouse.y + 'px';
+            element.style.top = mouse.y - window.scrollY + 'px';
             canvas.appendChild(element)
             canvas.style.cursor = "crosshair";
         }
