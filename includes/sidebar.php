114a62e59de6019c06f6a3554fdfa70213f44477
Nguyen Pham <baonguyenyam@gmail.com>
2021-06-08 10:39:08 -0500

    ok

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -72,5 +72,8 @@
                 </a>
             </li>
         </ul>
+        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+            <span style="text-transform: none">by Nguyen Pham</span>
+        </h6>
     </div>
 </nav>
