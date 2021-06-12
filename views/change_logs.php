b7293a4c8c3fa816bc6035267c45e04c0719590c
baonguyenyam <baonguyenyam@gmail.com>
2021-06-12 09:00:53 -0500

    sdfsdf

--- views/change_logs.php
+++ views/change_logs.php
@@ -17,7 +17,7 @@ require_once 'includes/header.php';
 
 
             <main class="col-md-9 ms-sm col-lg-10 col-xl px-md-4">
-                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
                     <h1 class="h2">Changelog</h1>
 
                 </div>
